b4973026553d99903c6f3e1cd3623b898e491b25
Anas <98676507+kt-anas@users.noreply.github.com>
2022-07-07 18:52:35 +0530

    Add files via upload

---
+++ js/java.js
@@ -0,0 +1,12 @@
+ const max = (numbers) =>{
+    let result = numbers[0]
+    for (const number of numbers) {
+       if(number > result) {
+        result = number
+       }
+    }
+    return {result}
+ }
+ console.log(max([1,2,4,5,6,71,1]))
+
+  
